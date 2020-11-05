88dd958af239c02b73293a137f7702265de3081e
Dércio Sinione Derone <derciosinione@gmail.com>
2020-11-05 16:38:42 +0200

    model NetworkCredential

--- Backend/api/User/models.py
+++ Backend/api/User/models.py
@@ -7,3 +7,11 @@ class Profile(models.Model):
     picture = models.ImageField('default.jpg')
     
 
+class NetworkCredential(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    social_network = models.CharField(verbose_name='Social Network',max_length=30,null=False,blank=False)
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    link = models.CharField(max_length=100,null=False,blank=False)
+    password = models.CharField(max_length=50)
+    
