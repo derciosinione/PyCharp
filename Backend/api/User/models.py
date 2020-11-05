c5361da4cfdd57db34d45ac958e2acbc899621fd
Dércio Sinione Derone <derciosinione@gmail.com>
2020-11-05 16:29:38 +0200

    model profile

--- Backend/api/User/models.py
+++ Backend/api/User/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.base import Model
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    picture = models.ImageField('default.jpg')
+    
+
