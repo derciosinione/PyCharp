b482ff7b453eeef2fbc42ad7db9a9dc09adda555
Dércio Sinione Derone <derciosinione@gmail.com>
2020-11-05 16:38:59 +0200

    model NetworkCredential

--- Backend/api/User/models.py
+++ Backend/api/User/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.base import Model
 
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
